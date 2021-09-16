266057a5eecd68f4279aeafe61b37ea20e2898be
OJ42 <88906115+OJ42@users.noreply.github.com>
2021-09-16 07:58:19 -0600

    Final product

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector("form").addEventListener("submit", addMovie);
+document.querySelector(`form`).addEventListener(`submit`, addMovie);
 const message = document.querySelector('#message')
 
 function addMovie(event) {
@@ -7,17 +7,17 @@ function addMovie(event) {
 
     const movie = document.createElement('li')
 
-    const movieTitle = document.createElement("span");
+    const movieTitle = document.createElement(`span`);
     movieTitle.textContent = inputField.value;
-    movieTitle.addEventListener("click", crossOffMovie);
+    movieTitle.addEventListener(`click`, crossOffMovie);
     movie.appendChild(movieTitle)
     
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "x";
-    deleteBtn.addEventListener("click", deleteMovie);
+    const deleteBtn = document.createElement(`button`);
+    deleteBtn.textContent = `x`;
+    deleteBtn.addEventListener(`click`, deleteMovie);
     movie.appendChild(deleteBtn);
     
-    const list = document.querySelector("ul");
+    const list = document.querySelector(`ul`);
     list.appendChild(movie);
 
     inputField.value = ''
@@ -25,16 +25,27 @@ function addMovie(event) {
 
 function deleteMovie(event) {
     
-    message.textContent = `Movie deleted!`
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`    
+    revealMessage()
     event.target.parentNode.remove();
+    
 }
 
 function crossOffMovie(event) {
     event.target.classList.toggle('checked')
 
     if (event.target.classList.contains('checked') === true) {
-        message.textContent = 'Movie watched!'
+        message.textContent = `${event.target.textContent} watched!`
     } else {
-        message.textContent = 'Movie added back!'
+        message.textContent = `${event.target.textContent} added back!`
     }
+    revealMessage()
+}
+
+function revealMessage() {
+    message.classList.remove('hide')
+    
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
 }
